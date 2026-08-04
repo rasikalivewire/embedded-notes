b9f3a4b430ff60fb1fbde160d502ea3f8abffbe6
rasikalivewire <rasikalivewire2026@gmail.com>
2026-08-04 13:16:12 +0530

    i2c code
    Updated I2C functions to use bit fields directly and modified configuration settings.

--- module 2:pic microcontrollerr/I2C/main.c
+++ module 2:pic microcontrollerr/I2C/main.c
@@ -1,81 +1,100 @@
-/*
- * File:   main.c
- * Author: Surya
- *
- * Created on 1 July, 2026, 9:11 AM
- */
-
-
 #include <xc.h>
 
 #define _XTAL_FREQ 20000000
 
-
+// CONFIGURATION BITS
 #pragma config FOSC = HS
 #pragma config WDTE = OFF
-#pragma config PWRTE = OFF
+#pragma config PWRTE = ON
 #pragma config BOREN = OFF
 #pragma config LVP = OFF
 #pragma config CPD = OFF
 #pragma config WRT = OFF
 #pragma config CP = OFF
 
-//---------------------- I2C Functions ----------------------//
+//----------------------------
+// Function Prototypes
+//----------------------------
+void I2C_Init(void);
+void I2C_Wait(void);
+void I2C_Start(void);
+void I2C_Stop(void);
+void I2C_Write(unsigned char data);
+
+//----------------------------
+// I2C Initialization
+//----------------------------
 void I2C_Init(void)
 {
-    TRISC3 = 1;              // SCL
-    TRISC4 = 1;              // SDA
+    TRISC3 = 1;      // SCL
+    TRISC4 = 1;      // SDA
 
-    SSPCON = 0x28;           // I2C Master Mode
+    SSPSTAT = 0x00;
+    SSPCON  = 0x28;      // I2C Master Mode
     SSPCON2 = 0x00;
-    SSPSTAT = 0x80;          // Slew rate disabled
-
-    SSPADD = 49;             // 100kHz I2C @20MHz
+    SSPADD  = 49;        // 100kHz @20MHz
 }
 
+//----------------------------
+// Wait Until Bus Free
+//----------------------------
 void I2C_Wait(void)
 {
-    while ((SSPCON2 & 0x1F) || SSPSTATbits.R_W);
+    while ((SSPCON2 & 0x1F) || SSPSTATbits.R_nW);
 }
 
+//----------------------------
+// START
+//----------------------------
 void I2C_Start(void)
 {
     I2C_Wait();
-    SSPCON2bits.SEN = 1;
+    SEN = 1;
+    while(SEN);
 }
 
+//----------------------------
+// STOP
+//----------------------------
 void I2C_Stop(void)
 {
     I2C_Wait();
-    SSPCON2bits.PEN = 1;
+    PEN = 1;
+    while(PEN);
 }
 
+//----------------------------
+// WRITE BYTE
+//----------------------------
 void I2C_Write(unsigned char data)
 {
     I2C_Wait();
+
     SSPBUF = data;
 
-    while (SSPSTATbits.BF);
-    I2C_Wait();
+    while(BF);
+    while(SSPSTATbits.R_nW);
 }
 
-//---------------------- Main ----------------------//
+//----------------------------
+// MAIN
+//----------------------------
 void main(void)
 {
     I2C_Init();
 
-    while (1)
+    while(1)
     {
+        __delay_ms(1000);
+
         I2C_Start();
 
-        I2C_Write(0xA0);      // Slave address (0x50 << 1) + Write
+        // Send Address
+        I2C_Write(0xA0);
 
-        I2C_Write(0xAA);      // Test Data 1
-        I2C_Write(0x55);      // Test Data 2
-        I2C_Write(0x12);      // Test Data 3
+        // Send Data
+        I2C_Write(0x55);
 
         I2C_Stop();
-
-        __delay_ms(1000);
     }
 }
